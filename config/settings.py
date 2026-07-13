87ad75afcf1ffe25e8c6c573da84fb2ff5da25c3
Jannatul Bakia <bakia.nimmy@gmail.com>
2026-07-13 14:14:24 +0600

    added postgre server

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # -----------------------------------------------------------------------------
@@ -97,13 +97,23 @@ WSGI_APPLICATION = "config.wsgi.application"
 # DATABASE
 # -----------------------------------------------------------------------------
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASE_URL = config("DATABASE_URL", default="")
 
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # -----------------------------------------------------------------------------
 # PASSWORD VALIDATION
 # -----------------------------------------------------------------------------
